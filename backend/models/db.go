a9affc4120a018afc6d0d826b86976c9ac4ad3e4
Hexdigest123 <pierre@merckel.dev>
2025-05-29 16:42:56 +0200

    CHG: transfered new db entries into db model

--- backend/models/db.go
+++ backend/models/db.go
@@ -65,3 +65,93 @@ type UserRole struct {
 	CreatedAt time.Time `db:"created_at" json:"createdAt"`
 	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"`
 }
+
+type Subnet struct {
+	ID        string         `db:"id" json:"id"`
+	Name      string         `db:"name" json:"name"`
+	Mask      sql.NullInt16  `db:"mask" json:"mask"`
+	Gateway   sql.NullString `db:"gateway" json:"gateway"`
+	DNS       sql.NullString `db:"dns" json:"dns"`
+	CreatedAt time.Time      `db:"created_at" json:"createdAt"`
+	UpdatedAt time.Time      `db:"updated_at" json:"updatedAt"`
+	CreatedBy string         `db:"created_by" json:"createdBy"`
+	UpdatedBy string         `db:"updated_by" json:"updatedBy"`
+}
+
+type DeviceRole struct {
+	ID          string         `db:"id" json:"id"`
+	Name        string         `db:"name" json:"name"`
+	Description sql.NullString `db:"description" json:"description"`
+	CreatedAt   time.Time      `db:"created_at" json:"createdAt"`
+	UpdatedAt   time.Time      `db:"updated_at" json:"updatedAt"`
+	CreatedBy   string         `db:"created_by" json:"createdBy"`
+	UpdatedBy   string         `db:"updated_by" json:"updatedBy"`
+}
+
+type Icon struct {
+	ID        string    `db:"id" json:"id"`
+	URL       string    `db:"url" json:"url"`
+	CreatedAt time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"`
+	CreatedBy string    `db:"created_by" json:"createdBy"`
+	UpdatedBy string    `db:"updated_by" json:"updatedBy"`
+}
+
+type OS struct {
+	ID          string         `db:"id" json:"id"`
+	Name        string         `db:"name" json:"name"`
+	Description sql.NullString `db:"description" json:"description"`
+	IconID      sql.NullString `db:"icon_id" json:"iconId"`
+	CreatedAt   time.Time      `db:"created_at" json:"createdAt"`
+	UpdatedAt   time.Time      `db:"updated_at" json:"updatedAt"`
+	CreatedBy   string         `db:"created_by" json:"createdBy"`
+	UpdatedBy   string         `db:"updated_by" json:"updatedBy"`
+}
+
+type Document struct {
+	ID        string    `db:"id" json:"id"`
+	URL       string    `db:"url" json:"url"`
+	MimeType  string    `db:"mime_type" json:"mimeType"`
+	Name      string    `db:"name" json:"name"`
+	CreatedAt time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"`
+	CreatedBy string    `db:"created_by" json:"createdBy"`
+	UpdatedBy string    `db:"updated_by" json:"updatedBy"`
+}
+
+type ServerStatus string
+
+const (
+	ServerStatusActive         ServerStatus = "ACTIVE"
+	ServerStatusInactive       ServerStatus = "INACTIVE"
+	ServerStatusMaintenance    ServerStatus = "MAINTENANCE"
+	ServerStatusProvisioning   ServerStatus = "PROVISIONING"
+	ServerStatusDecommissioned ServerStatus = "DECOMMISSIONED"
+)
+
+type Server struct {
+	ID        string       `db:"id" json:"id"`
+	Name      string       `db:"name" json:"name"`
+	Status    ServerStatus `db:"status" json:"status"`
+	IP        string       `db:"ip" json:"ip"`
+	SubnetID  string       `db:"subnet_id" json:"subnetId"`
+	OsID      string       `db:"os_id" json:"osId"`
+	CreatedAt time.Time    `db:"created_at" json:"createdAt"`
+	UpdatedAt time.Time    `db:"updated_at" json:"updatedAt"`
+	CreatedBy string       `db:"created_by" json:"createdBy"`
+	UpdatedBy string       `db:"updated_by" json:"updatedBy"`
+}
+
+type ServerRole struct {
+	ServerID  string    `db:"server_id" json:"serverId"`
+	RoleID    string    `db:"role_id" json:"roleId"`
+	CreatedAt time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt time.Time `db:"updated_at" json:"updatedAt"`
+}
+
+type ServerDocument struct {
+	ServerID   string    `db:"server_id" json:"serverId"`
+	DocumentID string    `db:"document_id" json:"documentId"`
+	CreatedAt  time.Time `db:"created_at" json:"createdAt"`
+	UpdatedAt  time.Time `db:"updated_at" json:"updatedAt"`
+}
